7ea67d4159eeccd97d1742f1eec0aab60a7efb9f
NkwaTambe <nkwajudetambe@gmail.com>
2024-10-18 14:40:25 +0100

    feat:Configured tailwindcss,daisy ui and otp page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
-import './App.css'
+import { useState } from 'react';
+import OtpInput from './components/OtpInput';
+import './App.css';
 
-function App() {
+export default function App() {
+  const [otp, setOtp] = useState('');
+  const onChange = (value: string) => setOtp(value);
+
   return (
     <>
-    <p><center>WEBANK</center></p>
+      <div className="container">
+        <h1>OTP Verification</h1>
+        <h4>Enter the verification code we just sent to your phone number</h4>
+        <OtpInput value={otp} valueLength={4} onChange={onChange} />
+      </div>
     </>
-  )
+  );
 }
-
-export default App
