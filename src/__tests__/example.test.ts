15bd004c5225138cac145db8f85ea1c2b09b45bd
NkwaTambe <nkwajudetambe@gmail.com>
2024-10-22 15:13:34 +0100

    feat:fix workflow by ensuring the use of proper code formate

--- src/__tests__/example.test.ts
+++ src/__tests__/example.test.ts
@@ -1,9 +1,9 @@
 // __tests__/example.test.ts
 
-import { describe, it, expect } from 'vitest'
+import { describe, it, expect } from "vitest";
 
-describe('Example Test', () => {
-  it('should pass a basic test', () => {
-    expect(true).toBe(true)
-  })
-})
+describe("Example Test", () => {
+  it("should pass a basic test", () => {
+    expect(true).toBe(true);
+  });
+});
