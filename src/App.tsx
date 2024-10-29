5cd85441055fbc42f1db47d3230906c1ef70e2fc
arielpetit <Ariel.Tchikaya@adorsys.com>
2024-10-29 16:18:18 +0100

    feat: generated key pairs on installation

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,41 @@
+// src/App.tsx
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Register from "./pages/RegisterPage";
 import OtpPage from "./pages/OtpPage";
 import DashboardPage from "./pages/DashboardPage";
+import Header from "./components/Header"; // Import the new Header component
+import { useEffect } from "react";
+import generateKeyPair from "./services/keyManagement/generateKey"; // Import the generate function
 import "./App.css";
+
 const App: React.FC = () => {
+  useEffect(() => {
+    const handleAppInstalled = async () => {
+      console.log("App installed successfully. Generating key pair...");
+
+      // Generate RSA key pair
+      await generateKeyPair();
+      console.log("Key pair generated successfully.");
+    };
+
+    // Listen for the appinstalled event
+    window.addEventListener("appinstalled", handleAppInstalled);
+
+    return () => window.removeEventListener("appinstalled", handleAppInstalled);
+  }, []);
+
   return (
     <Router>
-      <Routes>
-        {/* Define the routes */}
-        <Route path="/register" element={<Register />} />
-        <Route path="/otp" element={<OtpPage />} />
-        <Route path="/dashboard" element={<DashboardPage />} />
-      </Routes>
+      <div className="app">
+        <Header /> {/* Include the Header component */}
+        <Routes>
+          <Route path="/register" element={<Register />} />
+          <Route path="/otp" element={<OtpPage />} />
+          <Route path="/dashboard" element={<DashboardPage />} />
+        </Routes>
+      </div>
     </Router>
   );
 };
+
 export default App;
---
+++ src/services/keyManagement/generateKey.ts
@@ -0,0 +1,17 @@
+import * as jose from 'jose';
+
+async function generateKeyPair() {
+  // Generate an RSA key pair
+  const { publicKey, privateKey } = await jose.generateKeyPair('ES256',
+    { extractable: true })
+
+  // Convert keys to JSON format
+  const publicJWK = await jose.exportJWK(publicKey);
+  const privateJWK = await jose.exportJWK(privateKey);
+
+  console.log('Public Key:', publicJWK);
+  console.log('Private Key:', privateJWK);
+}
+
+
+export default generateKeyPair ;
