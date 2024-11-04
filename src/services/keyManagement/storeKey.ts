7e2e2d4d57329d92e5a1f99a6a49bf26a6a55bc4
arielpetit <Ariel.Tchikaya@adorsys.com>
2024-11-04 15:21:27 +0100

    fixed test using happy-dom as testing environment

--- src/services/keyManagement/storeKey.ts
+++ src/services/keyManagement/storeKey.ts
@@ -1,5 +1,5 @@
-import storage from "./storageSetup";
-import generateKeyPair from "./generateKey";
+import storage from "./storageSetup"; // Import the initialized storage
+import generateKeyPair from "./generateKey"; // Import your existing key generation function
 
 // Function to store a key pair in IndexedDB
 export async function storeKeyPair() {
