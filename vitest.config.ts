a9eb536dbf28b40cce1e108bad5621d2d8946f0e
Elwizzy12 <chiahans123@gmail.com>
2024-11-09 10:14:33 +0100

    fixed workflow for sonarqube

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
     environment: "happy-dom",
     coverage: {
       provider: "v8",
-      reporter: ["text", "json", "html"],
+      reporter: ["text", "json", "html","lcov"],
       all: true,
     },
     setupFiles: ["./vitest.setup.ts"],
