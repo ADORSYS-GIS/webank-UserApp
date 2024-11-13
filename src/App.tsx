a0e89558e56a643659ffd907b9a70c6c93e735df
Menkene Koufan <Menkene.Koufan@adorsys.com>
2024-11-13 10:24:16 +0100

    fix: made key pair always retrievable by making keyid fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,20 @@
 // src/App.tsx
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Register from "./pages/RegisterPage";
 import OtpPage from "./pages/OtpPage";
 import DashboardPage from "./pages/DashboardPage";
 import Header from "./components/Header";
 import "./App.css";
-import { FaroRoutes } from "@grafana/faro-react";
 
 const App: React.FC = () => {
   return (
     <Router>
       <Header />
-      <FaroRoutes>
+      <Routes>
         <Route path="/" element={<Register />} />
         <Route path="/otp" element={<OtpPage />} />
         <Route path="/dashboard" element={<DashboardPage />} />
-      </FaroRoutes>
+      </Routes>
     </Router>
   );
 };
