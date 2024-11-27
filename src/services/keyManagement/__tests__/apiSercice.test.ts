0184ae406974364efee50f135e4264816a072e0f
arielpetit <Ariel.Tchikaya@adorsys.com>
2024-11-27 15:28:35 +0100

    chore(): pwa optimasation

--- src/services/keyManagement/__tests__/apiSercice.test.ts
+++ src/services/keyManagement/__tests__/apiSercice.test.ts
@@ -1,12 +1,12 @@
 import axios from "axios";
 import MockAdapter from "axios-mock-adapter";
-import { sendOTP } from "../apiService";
+import { sendOTP, getAccountId } from "../apiService";
 import { getProjectEnvVariables } from "../../../shared/projectEnvVariables.ts";
 
 const { envVariables } = getProjectEnvVariables();
 
 describe("sendOTP", () => {
-  let mock: MockAdapter; // Explicitly declare the type
+  let mock: MockAdapter; 
 
   beforeEach(() => {
     mock = new MockAdapter(axios);
@@ -16,17 +16,17 @@ describe("sendOTP", () => {
     mock.restore();
   });
 
-  it("should send OTP successfully with valid inputs", async () => {
+  it("should send OTP successfully and extract account ID", async () => {
     const fullPhoneNumber = "1234567890";
     const jwtToken = "valid-token";
 
-    mock
-      .onPost(`${envVariables.VITE_BACKEND_URL}/registration`)
-      .reply(200, { message: "OTP sent" });
 
+    const mockResponse = "Registration successful for phone number: 1234567890. Account ID: 2LDztRXNQVQlCfxY3nXDPw%";
+    mock.onPost(`${envVariables.VITE_BACKEND_URL}/registration`).reply(200, mockResponse);
     const response = await sendOTP(fullPhoneNumber, jwtToken);
 
-    expect(response).toEqual({ message: "OTP sent" });
+    expect(response).toBe(mockResponse);
+    expect(getAccountId()).toBe("2LDztRXNQVQlCfxY3nXDPw%"); // Account ID extracted from response
   });
 
   it("should throw an error when API responds with an error", async () => {
--- vite.config.ts
+++ vite.config.ts
@@ -1,55 +1,86 @@
 import { VitePWA } from 'vite-plugin-pwa';
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
-  build: {
-    rollupOptions: {
-        output: {
-            format: 'es',
-            globals: {
-                react: 'React',
-                'react-dom': 'ReactDOM',
-            },
-            manualChunks(id) {
-                if (/projectEnvVariables.ts/.test(id)) {
-                    return 'projectEnvVariables'
-                }
-            },
-        },
-    },
-  },
-  plugins: [react(), VitePWA({
-    strategies: 'injectManifest',
-    srcDir: 'src',
-    filename: 'sw.ts',
-    registerType: 'autoUpdate',
-    injectRegister: false,
+  plugins: [
+    react(),
+    VitePWA({
+      strategies: 'injectManifest',
+      srcDir: 'src',
+      filename: 'sw.ts',
+      registerType: 'autoUpdate',
+      injectRegister: false,
 
-    pwaAssets: {
-      disabled: false,
-      config: true,
-    },
+      manifest: {
+        name: 'Webank',
+        short_name: 'Webank',
+        description: 'For regular users of Webank!',
+        theme_color: 'white',
+        background_color: 'white',
+        start_url: '/',
+        display_override: ['standalone', 'minimal-ui'],
+        display: 'standalone',
+        id: 'webank_user_app',
 
-    manifest: {
-      name: 'webank-UserApp',
-      short_name: 'UserApp',
-      description: 'For regular users of webank!',
-      theme_color: '#ffffff',
-    },
+        icons: [
+          {
+            src: '/android-chrome-144x144.png',
+            sizes: '144x144',
+            type: 'image/png',
+            purpose: "any",
+          },
+          {
+            src: '/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
 
-    injectManifest: {
-      globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
-    },
+        screenshots: [
+          {
+            src: '/Screenshot_otp.png',
+            type: 'image/png',
+            sizes: '472x923',
+            form_factor: 'wide',
+          },
+          {
+            src: '/Screenshot_dashboard.png',
+            type: 'image/png',
+            sizes: '472x923',
+            form_factor: 'wide',
+          },
+          {
+            src: '/Screenshot_register.png',
+            type: 'image/png',
+            sizes: '472x923',
 
-    devOptions: {
-      enabled: true,
-      navigateFallback: 'index.html',
-      suppressWarnings: true,
-      type: 'module',
-    },
-  })],
+            form_factor: 'wide',
+          },
+          {
+            src: '/Screenshot_dashboard.png',
+            type: 'image/png',
+            sizes: '472x923',
+            form_factor: 'narrow',
+          },
+        ],
+      },
 
-})
+      injectManifest: {
+        globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+      },
+
+      devOptions: {
+        enabled: true,
+        navigateFallback: 'index.html',
+        suppressWarnings: true,
+        type: 'module',
+      },
+    }),
+  ],
+});
