939fd7717cadcd3ec1c36a73e04f40f3f15a8089
yvanhenang <yvan.henang@skyengpro.com>
2025-02-14 09:37:41 +0100

    Update code

--- src/pages/TopUpPage.tsx
+++ src/pages/TopUpPage.tsx
@@ -1,21 +1,12 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import {calculateTransactionFee} from "../services/computation/transactionFeeCalculator";
 
 const TopUpPage: React.FC = () => {
   const [amount, setAmount] = useState<number | string>("");
   const navigate = useNavigate();
 
-  // Function to calculate the transaction fee based on the amount
-  const calculateTransactionFee = (amount: number) => {
-    if (amount <= 5000) {
-      return 0; // 0 XAF fee for amounts <= 5000 XAF
-    } else if (amount > 5000 && amount <= 500000) {
-      return 1000; // 1000 XAF fee for amounts between 5001 XAF and 500000 XAF
-    } else {
-      return 1000; // Default fee for amounts > 500000 XAF (not allowed)
-    }
-  };
-
+  
   const handleCancel = () => {
     navigate(-1); // Go back to the previous page
   };
