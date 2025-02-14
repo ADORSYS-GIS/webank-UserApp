27151de665a3a63ebc53cf887c2461a38606ac9d
Nancy Muyeh <nancymuyeh@gmail.com>
2025-02-14 11:44:05 +0100

    Create agent page

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import DashboardPage from "./pages/DashboardPage";
 import Header from "./components/Header";
 import "./App.css";
 import "react-toastify/dist/ReactToastify.css";
+import AgentPage from "./pages/Agentpage";
 
 const App: React.FC = () => {
   return (
@@ -15,6 +16,8 @@ const App: React.FC = () => {
         <Route path="/" element={<Register />} />
         <Route path="/otp" element={<OtpPage />} />
         <Route path="/dashboard" element={<DashboardPage />} />
+        <Route path="/agent" element={<AgentPage />} />
+
       </Routes>
     </HashRouter>
   );
