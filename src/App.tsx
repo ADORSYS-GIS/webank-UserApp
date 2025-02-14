95ba3914945de080b3b698329e558c74b570f011
NkwaTambe <nkwajudetambe@gmail.com>
2025-02-14 15:35:52 +0100

    merge conflict

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,10 @@ import { HashRouter, Routes, Route } from "react-router-dom";
 import Register from "./pages/RegisterPage";
 import OtpPage from "./pages/OtpPage";
 import DashboardPage from "./pages/DashboardPage";
-import TopUpPage from "./pages/TopUpPage"; // Importe la nouvelle page TopUpPage
 import Header from "./components/Header";
-import QRScanner from "./pages/QRScannerPage";
 import "./App.css";
 import "react-toastify/dist/ReactToastify.css";
+import QRScannerPage from "./pages/QRScannerPage";
 
 const App: React.FC = () => {
   return (
@@ -17,6 +16,7 @@ const App: React.FC = () => {
         <Route path="/" element={<Register />} />
         <Route path="/otp" element={<OtpPage />} />
         <Route path="/dashboard" element={<DashboardPage />} />
+        <Route path="/qr-scanner" element={<QRScannerPage />} />
       </Routes>
     </HashRouter>
   );
