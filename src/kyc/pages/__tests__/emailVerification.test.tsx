41f860febfaa396af964413f44f55513ae11471e
Menkene Koufan <Menkene.Koufan@adorsys.com>
2025-04-07 12:09:00 +0100

    improved settings page test with additional test case

--- src/kyc/pages/__tests__/emailVerification.test.tsx
+++ src/kyc/pages/__tests__/emailVerification.test.tsx
@@ -95,5 +95,6 @@ describe("InputEmail Component", () => {
     renderWithProviders(<InputEmail />);
     const backButton = screen.getByRole("button", { name: /Go Back/i });
     fireEvent.click(backButton);
+    expect(navigateMock).toHaveBeenCalledWith("/settings");
   });
 });
