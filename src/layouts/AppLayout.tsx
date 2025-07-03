4c33746e1cc08cc409b026cab0ccf71f45e5f08b
Menkene Koufan <Menkene.Koufan@adorsys.com>
2025-07-03 12:36:44 +0100

    fixed app layout by adding suspense

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -2,7 +2,9 @@ import Layout from "@shared/components/Layout";
 import Header from "@shared/components/Header";
 import KycCertChecker from "@features/kyc/pages/KycCertChecker";
 import { Toaster } from "sonner";
-import { ReactNode } from "react";
+import { ReactNode, Suspense } from "react";
+import { Outlet } from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 
 interface AppLayoutProps {
   children: ReactNode;
@@ -17,6 +19,15 @@ const AppLayout = ({ children }: AppLayoutProps) => {
       {children}
       <Toaster position="top-center" />
       {/* Add global popups, reminders, etc. here */}
+      {/* …your header, etc… */}
+      <Suspense fallback={<div>Loading…</div>}>
+        <Outlet />
+      </Suspense>
+
+      {/* Devtools tucked in under your layout */}
+      {import.meta.env.DEV && (
+        <TanStackRouterDevtools position="bottom-right" />
+      )}
     </Layout>
   );
 };
