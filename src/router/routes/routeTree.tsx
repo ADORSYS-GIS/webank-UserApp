bbd4cf74e79df18632aafc702ee7c0ca951b6713
Menkene Koufan <Menkene.Koufan@adorsys.com>
2025-07-04 12:55:54 +0100

    fixed sonar issue

--- src/router/routes/routeTree.tsx
+++ src/router/routes/routeTree.tsx
@@ -1,10 +1,9 @@
-import { createRootRoute, Outlet } from "@tanstack/react-router";
+import { createRootRoute, Outlet, Navigate } from "@tanstack/react-router";
 import { coreRoutesGroup } from "./coreRoutes";
 import { authRoutesGroup } from "./authRoutes";
 import { kycRoutesGroup } from "./kycRoutes";
 import { qrRoutesGroup } from "./qrRoutes";
 import { protectedRoutesGroup } from "./protectedRoutes";
-import { Navigate } from "@tanstack/react-router";
 
 export const rootRoute = createRootRoute({
   component: () => <Outlet />,
